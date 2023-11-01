4d3ca9643f7abbe58af27688d6be3ef3e1d2e735
GvozdevSV <agasfen82@yandex.ru>
2023-11-01 20:44:18 +0400

    add base_page.py

---
+++ pages/base_page.py
@@ -0,0 +1,76 @@
+import allure
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage:
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    @allure.step("открыть")
+    def open(self):
+        self.driver.get(self.url)
+
+    @allure.step("элемент видим")
+    def element_is_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
+    @allure.step("элементы видимы")
+    def elements_are_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    @allure.step("элемент представлен")
+    def element_is_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
+    @allure.step("элемент представлен долгое ожидание")
+    def element_is_present_long_wait(self, locator, timeout=20):
+        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
+    @allure.step("элементы представлены")
+    def elements_are_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    @allure.step("элемент не видим")
+    def element_is_not_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.invisibility_of_element(locator))
+
+    @allure.step("по элементу можно кликнуть")
+    def element_is_clicable(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+
+    @allure.step("перйти к элементу")
+    def go_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+
+    @allure.step("двойной клик")
+    def action_double_click(self, element):
+        action = ActionChains(self.driver)
+        action.double_click(element)
+        action.perform()
+
+    @allure.step("правый клик")
+    def action_right_click(self, element):
+        action = ActionChains(self.driver)
+        action.context_click(element)
+        action.perform()
+
+    @allure.step("перетянуть предмет на позицию")
+    def action_drag_and_drop_by_offset(self, element, x_coord, y_coord):
+        action = ActionChains(self.driver)
+        action.drag_and_drop_by_offset(element, x_coord, y_coord)
+        action.perform()
+
+    @allure.step("перетянуть предмет на элемент")
+    def action_drag_and_drop_to_element(self, what, where):
+        action = ActionChains(self.driver)
+        action.drag_and_drop(what, where)
+        action.perform()
+
+    @allure.step("перейти к элементу")
+    def action_move_to_element(self, element):
+        action = ActionChains(self.driver)
+        action.move_to_element(element)
+        action.perform()
