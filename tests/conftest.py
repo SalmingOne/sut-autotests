4285b56c71bc8aa405adeee62dfbb931b6dac3f0
sgvozdev <sgvozdev@web-bee.ru>
2023-11-02 19:22:32 +0400

    remove conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service as ChromeService, Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
