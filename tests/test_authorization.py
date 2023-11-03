3d62fb7d65096aee0547a8b64676d7b7542c56f7
sgvozdev <sgvozdev@web-bee.ru>
2023-11-03 06:53:10 +0400

    add in url

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,10 +1,10 @@
 from pages.authorization_page import AuthorizationPage
+from tests.conftest import IN_URL
 
 
 class TestAuthorization:
     def test_correct_authorization(self, driver):
-        authorization_page = AuthorizationPage(driver, 'http://10.7.2.3:33268/')
+        authorization_page = AuthorizationPage(driver, IN_URL)
         authorization_page.open()
         authorization_page.authorization('admin', 'password')
         authorization_page.check_authorization()
-
