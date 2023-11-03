b3af56c496d3d58843693442be8598a0e24fb5be
sgvozdev <sgvozdev@web-bee.ru>
2023-11-03 06:52:26 +0400

    add login

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService, Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from pages.authorization_page import AuthorizationPage
 
+IN_URL = 'http://10.7.2.3:33268/'
 @pytest.fixture(scope='function')
 def driver():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
     yield driver
     driver.quit()
+
+
+@pytest.fixture
+def login(driver):
+    authorization_page = AuthorizationPage(driver, IN_URL)
+    authorization_page.open()
+    authorization_page.authorization('admin', 'password')
