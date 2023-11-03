22c09da201e81910ab3cb9aeef095da889be1474
sgvozdev <sgvozdev@web-bee.ru>
2023-11-03 06:53:34 +0400

    add test

--- tests/test_create_project.py
+++ tests/test_create_project.py
@@ -1,6 +1,10 @@
+from pages.create_project_drawer_page import CreateProjectDrawerPage
+
 
 class TestCreateProject:
-    def test_create_project(self, driver):
+    def test_create_project(self,  login, driver):
+        create_project_drawer_page = CreateProjectDrawerPage(driver)
+        create_project_drawer_page.go_to_create_project_drawer_from_menu()
 
 
 
