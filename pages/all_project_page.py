b96506aa7fb68f4f17c4a989710a2b35731053bc
sgvozdev <sgvozdev@web-bee.ru>
2023-11-05 10:40:43 +0400

    add all_project_page.py

---
+++ pages/all_project_page.py
@@ -0,0 +1,20 @@
+from locators.all_project_page_locators import AllProjectPageLocators
+from pages.base_page import BasePage
+
+
+class AllProjectPage(BasePage):
+    locators = AllProjectPageLocators()
+
+    def go_to_all_project_page(self):
+        self.action_move_to_element(self.element_is_visible(self.locators.TAB_PROJECTS))
+        self.element_is_visible(self.locators.TAB_ALL_PROJECTS).click()
+
+    def check_project_name_at_all(self):
+        check_name_at_all = self.element_is_present(self.locators.CHECK_NAME_PROJECT).text
+        print(check_name_at_all)
+        return check_name_at_all
+
+
+
+
+
