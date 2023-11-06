81f967fba6c6879376fbfe98a311b918fb0eef15
sgvozdev <sgvozdev@web-bee.ru>
2023-11-06 11:09:56 +0400

    add data

--- tests/test_create_project.py
+++ tests/test_create_project.py
@@ -1,5 +1,7 @@
 import time
 
+import pytest
+
 from pages.all_project_page import AllProjectPage
 from pages.create_project_drawer_page import CreateProjectDrawerPage
 from pages.labor_cost_page import LaborCostPage
@@ -7,14 +9,15 @@ from pages.project_card_page import ProjectCardPage
 from tests import conftest
 
 
+@pytest.mark.smoke_test
 class TestCreateProject:
     # id-47 1.1.1 Создание нового проекта
     def test_create_project(self, login, driver):
-        #Создаем прект
+        # Создаем прект
         create_project_drawer_page = CreateProjectDrawerPage(driver)
         create_project_drawer_page.go_to_create_project_drawer_from_menu()
-        project_name, project_code, project_data, project_worker = create_project_drawer_page.create_project()
-        #Берм данные с карточки проекта
+        project_name, project_code, project_data, project_worker = create_project_drawer_page.create_project('no')
+        # Берем данные с карточки проекта
         project_card_page = ProjectCardPage(driver)
         project_card_page.go_to_description_tab()
         output_project_name, output_project_code, output_project_status, output_project_begin_data, output_project_manager = project_card_page.get_project_description()
@@ -22,17 +25,22 @@ class TestCreateProject:
         assert project_code == output_project_code, "поле код проекта не отобразилось в карточке проекта"
         assert project_data == output_project_begin_data, "поле дата начала проекта не отобразилось в карточке проекта"
         assert project_worker == output_project_manager, "поле администратор не отобразилось в карточке проекта"
-        #Берем имя поректа с старници все проекты
+        # Берем имя поректа с старници все проекты
         all_project_page = AllProjectPage(driver)
         all_project_page.go_to_all_project_page()
         check_name_at_all = all_project_page.check_project_name_at_all()
         assert project_name == check_name_at_all, "имя созданного проекта отсутствует на странице все проекты"
-        #Берем код проекта со страници трудозатрат
+        # Берем код проекта со страници трудозатрат
         labor_cost_page = LaborCostPage(driver)
         labor_cost_page.go_to_labor_cost_page()
         check_code_at_labor = labor_cost_page.check_project_code_at_labor()
         assert project_code == check_code_at_labor, "код созданного проекта отсутствует на странице трудозатрат"
+        # Пока удаление проекта здесь, планирую позже включить его в фикстуру
         all_project_page.go_to_all_project_page()
         all_project_page.delete_project()
 
+    def test_create_project_2(self, login, driver):
+        all_project_page = AllProjectPage(driver)
+        all_project_page.go_to_all_project_page()
+        all_project_page.delete_project()
         time.sleep(3)
