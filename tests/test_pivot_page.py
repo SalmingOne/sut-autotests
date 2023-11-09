399ce87cefe928d98b2994a381e43612f031db2e
sgvozdev <sgvozdev@web-bee.ru>
2023-11-09 21:57:42 +0400

    add id-3105

--- tests/test_pivot_page.py
+++ tests/test_pivot_page.py
@@ -46,3 +46,28 @@ class TestPivotPage:
         assert str(sum_in_month) == sum_on_user
         labor_cost_page.go_to_labor_cost_page()
         labor_cost_page.three_mont_clear()
+
+# id-3105 3.2.2.1 Проверка правильности подсчета трудозатрат за неделю при отображении "Сводной таблицы" "За неделю".
+    def test_correct_week_summ_on_pivot_for_week(self, login, driver):
+        # Создаем прект
+        # create_project_drawer_page = CreateProjectDrawerPage(driver)
+        # create_project_drawer_page.go_to_create_project_drawer_from_menu()
+        # create_project_drawer_page.create_project('no')
+        # Заполняем таблицу трудозатрат
+        labor_cost_page = LaborCostPage(driver)
+        labor_cost_page.go_to_labor_cost_page()
+        #sum_in_month = labor_cost_page.input_work_by_month()
+        print(sum_in_month)
+        # Берем сумму на сводной таблице проектов за месяц по неделям
+        pivot_tab_page = PivotTabPage(driver)
+        pivot_tab_page.go_to_pivot_page()
+        #pivot_tab_page.choose_period("month")
+        #sum_on_project = pivot_tab_page.get_sum_reason_on_project("month")
+        #assert str(sum_in_month) == sum_on_project
+        # Берем сумму на сводной таблице пользователей за месяц по неделям
+        pivot_tab_page.go_to_by_user_tab()
+        pivot_tab_page.open_project_list()
+        #sum_on_user = pivot_tab_page.get_sum_reason_on_user("month")
+        #assert str(sum_in_month) == sum_on_user
+        labor_cost_page.go_to_labor_cost_page()
+        labor_cost_page.three_mont_clear()
