00358d3bfa4c27eb4281e8dd14172e7d37de13ff
sgvozdev <sgvozdev@web-bee.ru>
2023-11-24 21:54:04 +0400

    feat(SUT-5164): автоматизирован тк-280

--- locators/create_local_user_drawer_locators.py
+++ locators/create_local_user_drawer_locators.py
@@ -11,12 +11,15 @@ class CreateLocalUserDrawerLocators:
     INPUT_NAME_FIELDS = (By.XPATH, '//input[contains(@class, "MuiOutlinedInput-input")][@name]')
     INPUT_PLACEHOLDER_FIELDS = (By.XPATH, '//input[contains(@class, "MuiOutlinedInput-input")][@placeholder]')
     HOUR_PAY_CHECKBOX = (By.XPATH, '//span[text()="Почасовая оплата"]')
+    # Локаторы вкладки проекты
     TAB_PROJECTS = (By.XPATH, '//button[text()="ПРОЕКТЫ"]')
-    TAB_CONTACTS = (By.XPATH, '//button[text()="КОНТАКТЫ"]')
     ADD_PROJECTS_BUTTON = (By.XPATH, '//button[text()="Добавить проект"]')
+    DELETE_PROJECTS_BUTTON = (By.CSS_SELECTOR, 'button[aria-label="Удалить проект"]')
     PROJECTS_FIELDS = (By.XPATH, '//label[contains(@class, "MuiInputLabel-outlined")]')
     PROJECT_MANAGER_CHECKBOX = (By.XPATH, '//span[text()="Руководитель проекта"]')
-
-
-
-
+    # Локаторы вкладки контакты
+    TAB_CONTACTS = (By.XPATH, '//button[text()="КОНТАКТЫ"]')
+    # Кнопки на всех вкладках
+    CLEAR_BUTTON = (By.CSS_SELECTOR, 'svg[data-testid="ClearIcon"]')
+    SAVE_BUTTON = (By.XPATH, '//button[contains(@class, "onboarding__save-button")]')
+    ABORT_BUTTON = (By.XPATH, '//button[text()="Отменить"]')
--- tests/test_create_local_user.py
+++ tests/test_create_local_user.py
@@ -1,18 +1,29 @@
+import allure
+
 from pages.create_local_user_drawer_page import CreateLocalUserDrawerPage
 
 
+@allure.suite("Дровер создания нового локального пользователя")
 class TestCreateLocalUser:
+
+    # id-280 4.2 Содержание дровера "Добавление нового пользователя" при создании локального пользователя
+    @allure.title("id-280 4.2 Содержание дровера Добавление нового пользователя при создании локального пользователя")
     def test_create_local_user_drawer(self, login, driver):
         create_local_user_page = CreateLocalUserDrawerPage(driver)
         create_local_user_page.go_to_create_local_user_drawer()
+        # Проверяем вкладку Личные данные
         create_local_user_page.check_names_text()
         create_local_user_page.check_placeholder_text()
         create_local_user_page.check_hour_pay_checkbox()
-
+        # Проверяем вкладку Проекты
         create_local_user_page.go_to_tab_projects()
         create_local_user_page.check_add_project_button_and_fields()
         create_local_user_page.check_project_manager_checkbox()
-
+        create_local_user_page.check_delete_project_button()
+        # Проверяем вкладку Контакты
         create_local_user_page.go_to_tab_contacts()
         create_local_user_page.check_names_on_contacts_text()
-
+        # Общие кнопки
+        create_local_user_page.check_clear_icon_button()
+        create_local_user_page.check_save_button()
+        create_local_user_page.check_abort_button()
