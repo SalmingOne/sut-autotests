9365d921505fb5a959abb4efeb425d1393846769
sgvozdev <sgvozdev@web-bee.ru>
2024-01-17 07:54:10 +0400

    feat(SUT-5384): добавлен тк, добавлены функции проверки элементов таблицы пользователи

--- locators/user_page_locators.py
+++ locators/user_page_locators.py
@@ -6,11 +6,13 @@ class UserPageLocators:
     SETTING_ICON = (By.CSS_SELECTOR, 'svg[data-testid="SettingsIcon"]')
     SYSTEM_ROLE = (By.CSS_SELECTOR, 'a[href="/admin/system-roles/users"]')
     ADD_LOCAL_USER_BUTTON = (By.XPATH, '//button[text()="Пользователь"]')
+    ADD_FREEIPA_USER_BUTTON = (By.XPATH, '//button[text()="Пользователь из Freeipa"]')
     # Поля поиска
     SEARCH_TAB_FIELDS = (By.CSS_SELECTOR, 'input[placeholder="Поиск"]')
     USER_STATUS = (By.XPATH, '//span[contains(@class, "MuiChip-label MuiChip-labelSmall")]')
     # Кебаб меню
     USER_KEBABS = (By.CSS_SELECTOR, 'svg[data-testid="MoreHorizIcon"]')
+    KEBAB_MENU_ITEM = (By.CSS_SELECTOR, 'span[class^="MuiTypography-root MuiTypography-caption"]')
     USER_FULL_INFO_BUTTON = (By.XPATH, '//span[text()="Полная информация"]')
     FIRED_BUTTON = (By.XPATH, '//span[text()="Уволить"]')
     RESTORE_BUTTON = (By.XPATH, '//span[text()="Восстановить"]')
@@ -25,5 +27,11 @@ class UserPageLocators:
     USER_CARD_TITLE = (By.XPATH, '//h6[text()="Просмотр полной информации"]')
     TAB_PROJECTS = (By.XPATH, '//button[text()="ПРОЕКТЫ"]')
     TAB_CONTACTS = (By.XPATH, '//button[text()="КОНТАКТЫ"]')
+
+    USER_PAGE_TITLE = (By.XPATH, '//h6[text()="Все пользователи"]')
+    FILTER_BUTTON = (By.XPATH, '//h6[text()="Отображение"]')
+    TAB_FILTER_BUTTONS = (By.XPATH, '//div[@class="ag-floating-filter-button"]/button')
+
+    COLUMNS_HEADERS = (By.CSS_SELECTOR, 'span[ref="eText"]')
 
 
