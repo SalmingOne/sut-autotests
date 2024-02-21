810d4c984a9fd5d975a5e9be07f7a67648c1e8cd
sgvozdev <sgvozdev@web-bee.ru>
2024-02-21 16:49:06 +0400

    feat(SUT-6626): добавлен тк Пустой ввод в обязательные для заполнения поля при редактировании таба Образование

---
+++ pages/user_profile_page.py
@@ -0,0 +1,58 @@
+import time
+
+import allure
+import testit
+
+from locators.user_profile_page_locators import UserProfilePageLocators
+from pages.base_page import BasePage
+
+
+class UserProfilePage(BasePage):
+    locators = UserProfilePageLocators()
+
+    @testit.step("Переходим в профиль пользователя")
+    @allure.step("Переходим в профиль пользователя")
+    def go_to_user_profile(self):
+        self.element_is_visible(self.locators.PROFILE_BUTTON).click()
+        self.element_is_visible(self.locators.MY_PROFILE_MENU_ITEM).click()
+
+    @testit.step("Переходим на вкладку образование")
+    @allure.step("Переходим на вкладку образование")
+    def go_to_education_tab(self):
+        self.element_is_visible(self.locators.EDUCATION_TAB_BUTTON).click()
+
+    @testit.step("Нажимаем кнопку редактировать")
+    @allure.step("Нажимаем кнопку редактировать")
+    def press_redact_button(self):
+        time.sleep(1)  # Без этого ожидания не всегда нажимается кнопка
+        self.element_is_visible(self.locators.REDACT_BUTTON).click()
+
+    @testit.step("Нажимаем иконку добавления нового диплома")
+    @allure.step("Нажимаем иконку добавления нового диплома")
+    def press_add_icon_button(self):
+        self.action_move_to_element(self.element_is_visible(self.locators.ADD_ICON))
+        self.element_is_visible(self.locators.ADD_ICON).click()
+
+    @testit.step("Нажимаем кнопку сохранения")
+    @allure.step("Нажимаем кнопку сохранения")
+    def press_save_button(self):
+        self.element_is_visible(self.locators.SAVE_BUTTON).click()
+
+    @testit.step("Берем текст сообщения системы")
+    @allure.step("Берем текст сообщения системы")
+    def get_alert_message(self):
+        return self.element_is_visible(self.locators.ALERT_TEXT).text
+
+    @testit.step("Берем цвет вкладки Образование")
+    @allure.step("Берем цвет вкладки Образование")
+    def get_tab_color(self):
+        return self.element_is_visible(self.locators.EDUCATION_TAB_BUTTON).value_of_css_property('background-color')
+
+    @testit.step("Берем текст ошибок с незаполненных обязательных полей")
+    @allure.step("Берем текст ошибок с незаполненных обязательных полей")
+    def get_mui_errors_text(self):
+        error_messages = self.elements_are_visible(self.locators.MUI_ERROR)
+        data = []
+        for message in error_messages:
+            data.append(message.text)
+        return data
