22214fb43d7e7e7e132a55487ed5e711c26f239c
sgvozdev <sgvozdev@ru-central1.internal>
2024-07-23 12:57:23 +0400

    fix(SUT-7625): доработана фикстура создания работающего и уволенного пользователя

---
+++ endpoints/system_roles_endpoint.py
@@ -0,0 +1,25 @@
+import allure
+import requests
+
+from data.urls import Urls
+from endpoints.auth_endpoint import AuthEndpoint
+
+
+class SystemRolesEndpoint:
+    response = None
+    response_json = None
+
+    @allure.step("Получаем все системные роли")
+    def get_all_system_roles(self):
+        header = AuthEndpoint().get_header_token_api()
+        self.response = requests.get(url=Urls.system_roles_url, headers=header)
+        self.response_json = self.response.json()
+        return self.response
+
+    @allure.step("Получаем id всех системных ролей")
+    def get_all_system_roles_id(self):
+        all_system_roles = self.get_all_system_roles().json()
+        system_roles_id = []
+        for role in all_system_roles:
+            system_roles_id.append(role['id'])
+        return system_roles_id
