ffbfe616e86b62a1300b06f3c08b32d27aa5fcb9
EvProtsik <evgenia.u.rzn@gmail.com>
2024-07-26 16:39:45 +0300

    feat(SUT-7619): добавлен тк переход на страницу пользователя из результатов поиска

--- locators/header_search_locators.py
+++ locators/header_search_locators.py
@@ -1,13 +1,25 @@
 from selenium.webdriver.common.by import By
 
 
+
+
 class HeaderSearchLocators:
     # Поле поиска в хедере
     SEARCH_HEADER_FIELD = (By.CSS_SELECTOR, 'input[placeholder="Поиск"]')
-    SEARCH_RESULTS_LIST = (By.CSS_SELECTOR, 'div[class="MuiBox-root css-1p7vog0"]')
+    SEARCH_RESULTS_LIST = (By.CSS_SELECTOR, 'ul[role="menu"]')
     SEARCH_RESULTS_PROJECTS = (By.XPATH, '//ul//div[text()="Проекты"]')
     SEARCH_RESULTS_USERS = (By.XPATH, '//ul//div[text()="Пользователи"]')
+    SEARCH_RESULTS_PROJECTS_LIST = (By.XPATH, '//li//a[contains(@href, "/projects/")]')
+    SEARCH_RESULTS_USERS_LIST = (By.XPATH, '//li//a[contains(@href, "/kk/")]')
 
     # Тултип
     TOOLTIP = (By.XPATH, '//div[contains(@class, "MuiTooltip-tooltipPlacementBottom")]')
     NOTHING_FOUND_TEXT = (By.XPATH, '//div[@role="tooltip"]//p')
+
+    # Страница результатов быстрого поиска
+    TAB_ALL = (By.XPATH, '//button[text()="ВСЕ"]')
+    TAB_USERS = (By.XPATH, '//button[text()="ПОЛЬЗОВАТЕЛИ"]')
+    TAB_PROJECTS = (By.XPATH, '//button[text()="ПРОЕКТЫ"]')
+    QUICK_SEARCH_USERS_LIST = (By.XPATH, '//div//a[contains(@href, "/kk/")]')
+    QUICK_SEARCH_PROJECTS_LIST = (By.XPATH, '//a[contains(@href, "/projects/")]//span')
+    GO_TO_USER_PAGE = (By.XPATH, '//div[text()="Информация о сотруднике"]/..')
--- pages/header_search_page.py
+++ pages/header_search_page.py
@@ -3,10 +3,10 @@ import time
 import allure
 import testit
 from selenium.webdriver import Keys
+from datetime import datetime
 
 from locators.header_search_locators import HeaderSearchLocators
 from pages.base_page import BasePage
-
 
 class HeaderSearchPage(BasePage):
     locators = HeaderSearchLocators()
@@ -32,5 +32,90 @@ class HeaderSearchPage(BasePage):
     @allure.step("Ввод пробела в поле поиска")
     def entering_a_space_in_the_header_search(self):
         self.element_is_visible(self.locators.SEARCH_HEADER_FIELD).send_keys(' ')
+
+    @testit.step("Ввод 2 символов в поле поиска")
+    @allure.step("Ввод 2 символов в поле поиска")
+    def entering_2_letters_in_header_search(self):
+        self.element_is_visible(self.locators.SEARCH_HEADER_FIELD).send_keys('пр')
+
+    @testit.step("Выпадающего списка нет")
+    @allure.step("Выпадающего списка нет")
+    def check_dropdown_not_visible(self):
+        self.element_is_not_visible(self.locators.SEARCH_RESULTS_LIST)
+
+    @testit.step("Ввод третьего символа в поле поиска")
+    @allure.step("Ввод третьего символа в поле поиска")
+    def entering_3th_letter_in_header_search(self):
+        self.element_is_visible(self.locators.SEARCH_HEADER_FIELD).send_keys('о')
+
+    @testit.step("Проектов в списке 2 и меньше")
+    @allure.step("Проектов в списке 2 и меньше")
+    def check_projects_in_dropdown_2_or_less(self):
+        projects_list = self.elements_are_visible(self.locators.SEARCH_RESULTS_PROJECTS_LIST)
+        projects = []
+        for project in projects_list:
+            projects.append(project.text)
+        assert len(projects) <= 2, "Проектов в дропдауне больше двух"
+
+    @testit.step("Пользователей в списке 2 и меньше")
+    @allure.step("Пользователей в списке 2 и меньше")
+    def check_users_in_dropdown_2_or_less(self):
+        users_list = self.elements_are_visible(self.locators.SEARCH_RESULTS_USERS_LIST)
+        users = []
+        for user in users_list:
+            users.append(user.text)
+        assert len(users) <= 2, "Пользователей в дропдауне больше двух"
+
+    @testit.step("Переход на страницу Быстрый поиск")
+    @allure.step("Переход на страницу Быстрый поиск")
+    def go_to_quick_search_page(self):
+        self.element_is_visible(self.locators.SEARCH_HEADER_FIELD).send_keys(Keys.ENTER)
+
+    @testit.step("Содержание страницы Быстрый поиск")
+    @allure.step("Содержание страницы Быстрый поиск")
+    def quick_search_page(self):
+        self.element_is_present(self.locators.TAB_ALL)
+        self.element_is_present(self.locators.TAB_USERS)
+        self.element_is_present(self.locators.TAB_PROJECTS)
+
+    @testit.step("Проверка отображения пользователей в алфавитном порядке")
+    @allure.step("Проверка отображения пользователей в алфавитном порядке")
+    def check_users_in_alphabetical_order(self):
+        users_list = self.elements_are_visible(self.locators.QUICK_SEARCH_USERS_LIST)
+        users = []
+        for user in users_list:
+            users.append(user.text)
+        assert users == sorted(users), "Пользователи не в алфавитном порядке"
+
+    @testit.step("Проверка отображения проектов в хронологическом порядке")
+    @allure.step("Проверка отображения проектов в хронологическом порядке")
+    def check_projects_in_chronological_order(self):
+        projects_list = self.elements_are_visible(self.locators.QUICK_SEARCH_PROJECTS_LIST)
+        projects = []
+        for project in projects_list:
+            projects.append(project.text)
+        list_prj = []
+        for x in projects:
+            list_prj.append(x[0:10])
+        new = []
+        for a in list_prj:
+            x = datetime.strptime(a, '%d.%m.%Y')
+            new.append(x)
+        assert new == sorted(new), 'Проекты не в хронологическом порядке'
+
+    @testit.step("Переход на страницу пользователя")
+    @allure.step("Переход на страницу пользователя")
+    def go_to_user_page_from_quick_search(self):
+        self.element_is_visible(self.locators.QUICK_SEARCH_USERS_LIST).click()
+
+    @testit.step("Проверка открытия страницы пользователя")
+    @allure.step("Проверка открытия страницы пользователя")
+    def check_page_after_redirect(self):
+        assert self.element_is_visible(self.locators.GO_TO_USER_PAGE), 'Страница пользователя не открылась'
+
+
+
+
+
 
 
