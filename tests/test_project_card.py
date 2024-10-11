a0003c9b63262e2a7bca232fcb9c8c4caef18d44
ivanovajulika <julika77@mail.ru>
2024-10-11 10:41:09 +0700

    feat(SUT-10075): добавлен тк отмена сохранения внесенных изменений в таблицу ресурсный план

--- tests/test_project_card.py
+++ tests/test_project_card.py
@@ -510,3 +510,31 @@ class TestProjectCard:
         table_after = project_card_page.displaying_table_resource_plan()
         # Проверяем что таблица не изменяется
         assert table_before == table_after, "Данные в таблице изменились после отмены внесения"
+
+    @testit.workItemIds(419)
+    @testit.displayName('2.1.1.1. Отмена сохранения внесенных изменений в таблицу "Ресурсный план"')
+    @pytest.mark.regress
+    @allure.title('id-419 2.1.1.1. Отмена сохранения внесенных изменений в таблицу "Ресурсный план"')
+    def test_drover_cancel_adding_attraction_period(self, simple_project, login, driver):
+        all_project_page = AllProjectPage(driver)
+        all_project_page.go_to_all_project_page()
+        all_project_page.go_project_page(simple_project['name'])
+        project_card_page = ProjectCardPage(driver)
+        project_card_page.go_to_resource_plan_tab()
+        project_card_page.change_radiobutton()
+        # Получаем отображение таблицы "Ресурсный план" до изменений
+        table_before = project_card_page.displaying_table_resource_plan()
+        project_card_page.press_add_percent_button()
+        project_card_page.set_period_and_percentage()
+        project_card_page.press_save_in_drover()
+        # Получаем отображение таблицы "Ресурсный план" после изменений
+        table_after = project_card_page.displaying_table_resource_plan()
+        # Проверяем что таблица изменяется
+        assert table_before != table_after
+        time.sleep(10)
+        project_card_page.press_break_button()
+        # Получаем отображение таблицы "Ресурсный план" после отмены сохранения
+        table_after_cancel = project_card_page.displaying_table_resource_plan()
+        # Проверяем что таблица вернулась к изначальному состоянию
+        assert table_before == table_after_cancel
+        time.sleep(10)
