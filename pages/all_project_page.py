4524fe878689a41949b2c76fdc53757b10d3d788
ivanovajulika <julika77@mail.ru>
2024-10-24 20:40:47 +0700

    pr(SUT-10422): исправлены ошибки по комментариям

--- pages/all_project_page.py
+++ pages/all_project_page.py
@@ -17,12 +17,13 @@ class AllProjectPage(BasePage):
         time.sleep(2)
         self.action_move_to_element(self.element_is_visible(self.locators.TAB_PROJECTS))
         self.element_is_visible(self.locators.TAB_ALL_PROJECTS).click()
+        self.element_is_visible(self.locators.KEBAB_MENU, 10)
 
     @testit.step("Проверяем, что имя проекта есть на странице")
     @allure.step("Проверяем, что имя проекта есть на странице")
     def check_project_name_at_all(self, project_name):
         assert self.element_is_displayed(self.locators.check_project_name_on_tab(project_name)), \
-            "имя созданного проекта отсутствует на странице все проекты"
+            "Имя созданного проекта отсутствует на странице все проекты"
 
     @testit.step("Удаляем проект")
     @allure.step("Удаляем проект")
