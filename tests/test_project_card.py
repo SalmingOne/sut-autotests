7351ca09bb222ac8b83156287172b0625c88aed1
ivanovajulika <julika77@mail.ru>
2024-11-05 20:00:42 +0700

    feat(SUT-10471): добавлен тк отображение часов при заполнении таблицы во временном интервале Год

--- tests/test_project_card.py
+++ tests/test_project_card.py
@@ -924,4 +924,30 @@ class TestProjectCard:
         list_year = project_card_page.displaying_table_resource_plan()
         assert sum(list(map(int, [x for x in list_year if x != '-']))) == sum(list(map(int, [x for x in list_month if x != '-']))), \
             'Сумма часов по дням за месяц не равна значению часов в месяц'
+        
+    @testit.workItemIds(11785)
+    @testit.displayName('2.1.2.2 Отображение часов при заполнении таблицы во временном интервале "Год"')
+    @pytest.mark.regress
+    @allure.title('id-11785 2.1.2.2 Отображение часов при заполнении таблицы во временном интервале "Год"')
+    def test_displaying_hours_when_table_full_in_year(self, simple_project, login, driver):
+        all_project_page = AllProjectPage(driver)
+        all_project_page.go_to_all_project_page()
+        all_project_page.go_project_page(simple_project['name'])
+        project_card_page = ProjectCardPage(driver)
+        project_card_page.go_to_resource_plan_tab()
+        project_card_page.chose_period('Год')
+        project_card_page.press_add_employment_button()
+        start_date, end_date = project_card_page.get_current_month_start_end()
+        project_card_page.set_period_and_busy(start_date.strftime("%d.%m.%Y"), end_date.strftime("%d.%m.%Y"))
+        project_card_page.press_save_in_drover()
+        list_year = project_card_page.displaying_table_resource_plan()
+        project_card_page.chose_period('Квартал')
+        list_quarter = project_card_page.displaying_table_resource_plan()
+        list_quarter = list(map(int, [x for x in list_quarter if x != '-' and x != '0']))
+        project_card_page.chose_period('Месяц (по дням)')
+        list_month = project_card_page.displaying_table_resource_plan()
+        list_week = project_card_page.converting_list_hours_day_to_list_week(list_month)
+        assert list_quarter == list_week, 'Сумма часов по дням за неделю не равна значению часов в неделю'
+        assert sum(list(map(int, [x for x in list_year if x != '-']))) == sum(list(map(int, [x for x in list_month if x != '-']))), \
+            'Сумма часов по дням за месяц не равна значению часов в месяц'
     
