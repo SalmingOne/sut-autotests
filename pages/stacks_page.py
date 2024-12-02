5cf66e36f0fcc7c064c0f0fb013754e83b931ab0
sgvozdev <agasfen82@yandex.ru>
2024-12-02 11:25:40 +0400

    feat(SUT-10960): добавлен чек-лист негативные проверки при редактировании стека

--- pages/stacks_page.py
+++ pages/stacks_page.py
@@ -1,5 +1,7 @@
 import time
 
+from selenium.webdriver import Keys
+
 from locators.stacks_page_locators import StacksPageLocators
 from pages.base_page import BasePage
 from utils.concat_testit_allure_step import allure_testit_step
@@ -261,3 +263,25 @@ class StacksPage(BasePage):
     @allure_testit_step('Проверка наличия стека на странице')
     def check_stack_name_on_page(self, name):
         return self.element_is_displayed(self.locators.stack_name_on_page(name))
+
+    @allure_testit_step('Негативные проверки поля Стек')
+    def check_name_field_when_redact(self):
+        self.action_select_all_text(self.element_is_visible(self.locators.NAME_FIELD))
+        self.element_is_visible(self.locators.NAME_FIELD).send_keys(Keys.BACK_SPACE)
+        self.element_is_visible(self.locators.text_on_page('Действия')).click()
+        assert self.element_is_visible(self.locators.MUI_ERROR).text == 'Поле обязательно', \
+            "Нет сообщения об обязательности поля"
+        name_field_color = self.element_is_present(self.locators.NAME_FIELD_COLOR).value_of_css_property('border-color')
+        assert name_field_color == 'rgb(211, 47, 47)', "Цвет поля Стек не красный"
+        time.sleep(1)
+        assert not self.element_is_clickable(self.locators.SUBMIT_BUTTON, 2), \
+            "Кнопка Сохранить активна при не корректном заполнении поля Стек"
+        error = self.check_max_field_length(
+            self.locators.NAME_FIELD,
+            255,
+            self.locators.NAME_FIELD_COLOR,
+            self.locators.text_on_page('Действия'),
+            'AAA'
+        )
+        assert error == 'Превышено допустимое количество символов: 255', \
+            "Не появилось сообщение о превышении допустимого количества символов в поле Стек"
